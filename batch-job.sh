028166e6c475535d304bbc689477e2af24e5df1a
Hao Xiong <haoxiong@outlook.com>
2019-06-26 17:47:48 -0500

    add epoch to command line

--- batch-job.sh
+++ batch-job.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-parallel --bar --retry-failed --joblog logs/log -j1 \
-'python run.py -n={1} -k={2} -d={3} -b={4} -r={5} -c={6} --ema -s={7} --device=0' ::: \
-nltcs kdd audio netflix jester ::: \
-50 70 100 ::: \
-10 30 50 ::: \
-64 128 ::: \
-0.005 0.001 ::: \
-0.1 0.2 0.5 ::: \
-17 23
+parallel --bar --retry-failed --joblog logs/log1 -j1 \
+'python run.py -n={1} -k={2} -d={3} -b={4} -e={5} -r={6} -c={7} --ema -s={8} --device=0' ::: \
+50-17-8 ::: \
+100 ::: \
+50 70 ::: \
+100 ::: \
+300 ::: \
+0.0005 ::: \
+0.1 ::: \
+1
 ###ad accidents audio bbc netflix book 20ng cr52 webkb dna jester kdd kosarek msnbc msweb nltcs plants pumsb_star tmovie tretail ::: \
 #retail webkb dna kosarek  ::: \
