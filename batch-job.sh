323d7fd5bee2a0876c05944524c71e6df20eb82e
Hao Xiong <haoxiong@outlook.com>
2019-07-09 13:47:09 -0500

    add batch jobs for another 4 datasets

--- batch-job.sh
+++ batch-job.sh
@@ -16,16 +16,16 @@
 #webkb(839) kosarek(190) retail(135) audio(100) netflix(100) jester(100) kdd(64) nltcs(16)
 #-------------------------------------------------------------------------------------
 
-parallel --bar --retry-failed --joblog logs/log_ranj -j2 \
-'python run.py --name={1} -k={2} --dim={3} --batch={4} --epoch={5} --rate={6} --cost={7} --seed={8} --device=0 --ema' ::: \
-kosarek retail audio netflix jester ::: \
-100 150 ::: \
-50 100 ::: \
-128 ::: \
-250 ::: \
-0.0005 0.001 ::: \
-1 0.25 0.5 ::: \
-12
+#parallel --bar --retry-failed --joblog logs/log_ranj -j2 \
+#'python run.py --name={1} -k={2} --dim={3} --batch={4} --epoch={5} --rate={6} --cost={7} --seed={8} --device=0 --ema' ::: \
+#kosarek retail audio netflix jester ::: \
+#100 150 ::: \
+#50 100 ::: \
+#128 ::: \
+#250 ::: \
+#0.0005 0.001 ::: \
+#1 0.25 0.5 ::: \
+#12
 
 #-------------------------------------------------------------------------------------
 
@@ -39,3 +39,14 @@ kosarek retail audio netflix jester ::: \
 #0.0005 0.001 ::: \
 #0.25 0.5 1 ::: \
 #17
+
+parallel --bar --retry-failed --joblog logs/log_stu -j2 \
+'python run.py --name={1} -k={2} --dim={3} --batch={4} --epoch={5} --rate={6} --cost={7} --seed={8} --device=-1 --ema' ::: \
+50-17-8 students_03_02-0000 ::: \
+200 ::: \
+70 100 ::: \
+128 ::: \
+250 ::: \
+0.0005 0.001 ::: \
+0.25 0.5 1 ::: \
+7
